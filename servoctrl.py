bb13cb953433a7d0d5cb52e563bb847baea05dc7
AnirudhMaheshwaram <55203441+AnirudhMaheshwaram@users.noreply.github.com>
2020-04-02 06:05:34 -0400

    Create servoctrl.py
    can run with system arguments
    to run give make file executable by
    sudo chmod +x servoctrl.py
    Then run it by typing in ./servoctrl.py followed by 1 or 0 for example
    ./servoctrl.py 1

---
+++ servoctrl.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+import Adafruit_BBIO.PWM as PWM
+import time
+import sys
+#servo pin number
+servo_pin = "P8_13"
+#stores first argument while calling program
+user_input = int(sys.argv[1])
+if user_input == 1:
+        #executes if user input =1
+        print("opened")
+        #duty cycles=97 for opening ~0 degrees
+        PWM.start(servo_pin, 92, 60, 1)
+        #need time gap to execute previous cmd
+        time.sleep(1)
+        #stops pwm at servo pin
+        PWM.stop(servo_pin)
+        #cleans up inmported pwm functions from memory
+        PWM.cleanup()
+else:
+        #runs when user input is 0 for close
+        PWM.start(servo_pin, 97.4, 60, 1)
+        #time gap to execute code
+        time.sleep(1)
+        #stops pwm
+        PWM.stop(servo_pin)
+        #cleans memory
+        PWM.cleanup()
+        print("closed")
