43c3264684520acfb5e94cd12d0dc95d82fc80e9
Mani-Muvvala <55203258+Mani-Muvvala@users.noreply.github.com>
2020-04-16 11:26:52 -0400

    Update get-latest-item-CrowdDensity.js

--- AWS/Lambda/get-latest-item-CrowdDensity.js
+++ AWS/Lambda/get-latest-item-CrowdDensity.js
@@ -2,35 +2,45 @@ const AWS = require('aws-sdk')
 const docClient = new AWS.DynamoDB.DocumentClient()
 
 // async function abstraction
-async function getItem(id){
-  var params = {
-    TableName : 'crowd_density_frequent',
-    Key: { date_string: '99999999999999' }
+async function getItem(id)
+{
+  var params = 
+      {
+    TableName : 'crowd_density_frequent',                        // dynamodb table name
+    Key: { date_string: '99999999999999' }                       // key itemusing which we will access
   }
-  try {
-    const data = await docClient.get(params).promise()
-    return data
+  try 
+  {
+    const data = await docClient.get(params).promise()           // getting data
+    return data                                                  // returns data
     
-  } catch (err) {
-    return err
+  } 
+  catch (err) 
+  {
+    return err                                                  // returns error
   }
 }
 
 // usage
 exports.handler = async (event, context) => {
-  try {
-    const data = await getItem()
-    const response = {
+  try 
+  {
+    const data = await getItem()                                // waiting for item to get
+    const response = 
+          {
         statusCode: 200,
-        headers: {
+        headers: 
+            {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Headers": "*",
           "Access-Control-Allow-Methods": "*"
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data),                             // converts java script value to json format
     };
-    context.succeed(response);
-  } catch (err) {
+    context.succeed(response);                                  
+  } 
+  catch (err) 
+  {
     return { error: err }
   }
 }
